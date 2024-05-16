af7cd1ded7e015874ac447d5ad9e0707805c9fd1
Paulo <pdichone@gmail.com>
2024-05-16 16:04:30 -0700

    add navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+/** @format */
+
+function NavBar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          BookShelf
+        </a>
+      </div>
+    </nav>
+  );
+}
+export default NavBar;
