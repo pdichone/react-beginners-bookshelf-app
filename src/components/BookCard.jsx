1bfc17b2c9274db8de3a5de3d2e2e13b85b68d19
Paulo <pdichone@gmail.com>
2024-05-20 14:19:06 -0700

    handling api missing fields

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,17 +1,23 @@
 /** @format */
 import 'react-loading-skeleton/dist/skeleton.css';
 function BookCard({ book, onSeeMore }) {
+  const {
+    title = 'No Title Available',
+    authors = ['Unknown Authors'],
+    imageLinks = {},
+  } = book.volumeInfo;
+  const thumbnail = imageLinks.thumbnail || 'https://placehold.co/180x280';
+
   return (
     <div className="col-md-4 mb-3">
       <div className="card">
-        <img
-          className="card-img-top"
-          src={book.volumeInfo.imageLinks.thumbnail}
-          alt={book.volumeInfo.title}
-        />
+        <img className="card-img-top" src={thumbnail} alt={title} />
         <div className="card-body">
-          <p className="card-text">{book.volumeInfo.authors.join(', ')}</p>
-          <button onClick={() => onSeeMore(book)} className="btn btn-primary"> See More</button>
+          <p className="card-text">{authors.join(', ')}</p>
+          <button onClick={() => onSeeMore(book)} className="btn btn-primary">
+            {' '}
+            See More
+          </button>
         </div>
       </div>
     </div>
--- src/components/BookDetail.jsx
+++ src/components/BookDetail.jsx
@@ -1,12 +1,20 @@
 /** @format */
 
 function BookDetail({ book, onClose }) {
+  const {
+    title = 'No Title Available',
+    authors = ['Unknown Authors'],
+    imageLinks = {},
+    description = 'No description available',
+  } = book;
+  const thumbnail = imageLinks.thumbnail || 'https://placehold.co/180x280';
+
   return (
     <div className="modal show d-block" tabIndex="-1" role="dialog">
       <div className="modal-dialog" role="document">
         <div className="modal-content">
           <div className="modal-header">
-            <h5 className="modal-title">{book.title}</h5>
+            <h5 className="modal-title">{title}</h5>
             {/* <button
               className="btn btn-danger"
               type="button"
@@ -17,19 +25,15 @@ function BookDetail({ book, onClose }) {
             </button> */}
           </div>
           <div className="modal-body">
-            <img
-              src={book.imageLinks?.thumbnail}
-              alt={book.title}
-              className="img-fluid mb-3"
-            />
+            <img src={thumbnail} alt={title} className="img-fluid mb-3" />
             <p>
-              <strong>Authors:</strong> {book.authors.join(', ')}
+              <strong>Authors:</strong> {authors.join(', ')}
             </p>
             <p>
               <strong>Published Date:</strong> {book.publishedDate}
             </p>
             <p>
-              <strong>Description:</strong> {book.description}
+              <strong>Description:</strong> {description}
             </p>
           </div>
           <div className="modal-footer">
